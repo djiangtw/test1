08207855adb590f83d072e33c7aed798b948a31e
Danny Jiang <xinyi904314@yahoo.com.tw>
2016-01-12 10:12:25 +0800

    add cc

---
+++ c/cc.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cc.c
+ *
+ *    Description:  test code
+ *
+ *        Version:  1.0
+ *        Created:  2016年01月12日 10時11分06秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("%s\n", "CC, this is a test");
+    return 0;
+}
